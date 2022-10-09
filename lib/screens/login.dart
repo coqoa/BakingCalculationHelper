e83b25fe8cec2102a845bdfc7ef97a3107a19248
ChoiByeongMin <coqoa28@gmail.com>
2022-10-09 23:49:24 +0900

    221009 유효성체크

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,6 +5,7 @@ import 'package:baking_calculation_helper/controller/controller.dart';
 import 'package:baking_calculation_helper/screens/main_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
@@ -35,20 +36,34 @@ class _LoginState extends State<Login> {
   // 텍스트필드 클래스화하기
   // 코드 정리하기
 
-  // 어디에 사용하는 변수?
+  // db에 ID 있는지 검증하는 변수
   bool signupIdDefault = true;
-  // 어디에 사용하는 변수?
+
+  // 어디에 쓰는 변수?
+  bool signupPasswordValidator = true;
+  bool signupPasswordCheckValidator = true;
+  String validatorValue = '';
+
+  // 사인업 페이지 각 항목 유효성 체크 결과
   String signupIdValidateValue = '';
-  // Signup Id Validate Function
+  String signupPasswordValidateValue = '';
+  String signupPasswordCheckValidateValue = '';
+
+  // 사인업 아이디 유효성 검사 함수
   void signupIdValidateFunc(userEmail, signupIdDefault){
+    if(userEmail.length < 1){
+      setState(() {
+        signupIdValidateValue = '이메일을 입력해주세요';
+      });
+    }
     if(!userEmail.contains('@')){
       setState(() {
-        signupIdValidateValue = '이메일 형식으로 작성해주세요';
+        signupIdValidateValue = '이메일 형식으로 작성해주세요 (예시 : abc@example.com)';
       });
     }
     if(!userEmail.contains('.')){
       setState(() {
-        signupIdValidateValue = '이메일 형식으로 작성해주세요';
+        signupIdValidateValue = '이메일 형식으로 작성해주세요 (예시 : abc@example.com)';
       });
     }
     if(userEmail.contains('@') && userEmail.contains('.')){
@@ -61,38 +76,33 @@ class _LoginState extends State<Login> {
         signupIdValidateValue = '존재하는 아이디 입니다';
       });
     }
+    
   }
 
-  // Firebase Auth Error Handling Function
-  void authErrorHandlingfunc(String errorCode){
-    switch (errorCode) {
-      case '[firebase_auth/invalid-email] The email address is badly formatted.':
-          setState(() {
-            validatorValue = '이메일형식지켜';
-          });
-          log('validatorValue');
-          print(validatorValue);
-        break;
-      case '[firebase_auth/missing-email] Error':
-          setState(() {
-            validatorValue = '에러다임마';
-          });
-          log('validatorValue');
-          print(validatorValue);
-        break;
+  // // Firebase Auth Error Handling Function
+  // void authErrorHandlingfunc(String errorCode){
+  //   switch (errorCode) {
+  //     case '[firebase_auth/invalid-email] The email address is badly formatted.':
+  //         setState(() {
+  //           validatorValue = '이메일형식지켜';
+  //         });
+  //         log('validatorValue');
+  //         print(validatorValue);
+  //       break;
+  //     case '[firebase_auth/missing-email] Error':
+  //         setState(() {
+  //           validatorValue = '에러다임마';
+  //         });
+  //         log('validatorValue');
+  //         print(validatorValue);
+  //       break;
 
-      default:
-        log('---------------------------errorCode--------------------------');
-        print(errorCode);
-        log('---------------------------errorCode--------------------------');
-    }
-  }
-
-
-  bool signupPasswordValidator = true;
-  bool signupPasswordCheckValidator = true;
-
-  String validatorValue = '';
+  //     default:
+  //       log('---------------------------errorCode--------------------------');
+  //       print(errorCode);
+  //       log('---------------------------errorCode--------------------------');
+  //   }
+  // }
 
   void _tryValudation(){
     final isValid = _formKey.currentState!.validate();
@@ -110,14 +120,13 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Palette.red,
       body: Center(
         child: AnimatedContainer(
           duration: Duration(milliseconds: 500),
           curve: Curves.easeIn,
           width: MediaQuery.of(context).size.width< 1950 
-          ? MediaQuery.of(context).size.width*0.6 
-          : MediaQuery.of(context).size.width*0.5,
+            ? MediaQuery.of(context).size.width*0.6 
+            : MediaQuery.of(context).size.width*0.5,
           height: !isSignupScreen ? 300 : 400,
           // color: Colors.green,
           padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
@@ -132,7 +141,6 @@ class _LoginState extends State<Login> {
               )
             ]
           ),
-
           
           child: SingleChildScrollView(
             child: Column(
@@ -314,19 +322,20 @@ class _LoginState extends State<Login> {
                       children: [
                         // 아이디
                         TextFormField(
+                          inputFormatters: [FilteringTextInputFormatter.allow(RegExp('[0-9,a-z,A-Z,@,.]'))], // 정규식
                           autofocus: true,
                           keyboardType: TextInputType.emailAddress,
                           key: ValueKey(3),
-                          validator: (value){
-                            // 유효성검사
-                            if(value!.isEmpty || value.length < 4){
-                              return 'Please enter at least 4 characters';
-                            }
-                            return null;
-                          },
-                          onSaved: (value){
-                            userEmail = value!;
-                          },
+                          // validator: (value){
+                          //   // 유효성검사
+                          //   if(value!.isEmpty || value.length < 4){
+                          //     return 'Please enter at least 4 characters';
+                          //   }
+                          //   return null;
+                          // },
+                          // onSaved: (value){
+                          //   userEmail = value!;
+                          // },
 
                           onChanged: (value){
                             userEmail = value;
@@ -350,74 +359,61 @@ class _LoginState extends State<Login> {
                           },
                           style: TextStyle(
                             color: signupIdDefault ? Palette.black : Palette.red 
-                            // color: (){
-                            //     // signupIdDefault ? Palette.iconColor : Palette.red 
-                            //     // 그린 표시
-                            //     if(signupIdDefault){
-                            //       if(signupIdValidator){
-                            //         Palette.green;
-                            //       }else{
-                            //         Palette.iconColor;
-                            //       }
-                            //     }else if(!signupIdDefault){
-                            //       Palette.red;
-                            //     }
-                            //   }()
                           ),
                           decoration: InputDecoration(
                             prefixIcon: Icon(
                               Icons.account_circle,
-                              color: signupIdDefault ? Palette.iconColor : Palette.red 
+                              color: Palette.iconColor
                             ),
                             enabledBorder: OutlineInputBorder(
                               borderSide: BorderSide(
-                                color: signupIdDefault ? Palette.textColor1 : Palette.red 
+                                color: Palette.textColor1
                               ),
                               borderRadius: BorderRadius.circular(25)
                             ),
                             focusedBorder: OutlineInputBorder(
                               borderSide: BorderSide(
-                                color: signupIdDefault ? Palette.blue : Palette.red 
+                                // color: signupIdDefault ? Palette.blue : Palette.red 
+                                color: Palette.blue
                               ),
                               borderRadius: BorderRadius.circular(25)
                             ),
                             hintText: 'E-Mail',
                             hintStyle: TextStyle(
                               fontSize: 14,
+                              fontFamily: "NotoSansRegular",
                               color: Palette.textColor1
                             ),
                             contentPadding: EdgeInsets.all(10.0)
                           ),
                         ),
 
+                        // 회원가입 아이디 검증 결과
                         Container(
-                          height: 20,
-
-                          // child: Text(signupIdDefault ? '' : '존재하는 아이디 입니다',
-                          //   style: TextStyle(
-                          //     fontSize: 12,
-                          //     fontFamily: "NotoSansRegular",
-                          //     color: Colors.red[400],
-                          //     // fontWeight: FontWeight.bold,
-                          //   ),
-                          // ),  
-
-                          child: Text(signupIdValidateValue),
+                          height: 30,
+                          child: Text(signupIdValidateValue,
+                           style: TextStyle(
+                              fontSize: 12,
+                              fontFamily: "NotoSansRegular",
+                              color: Colors.red[400],
+                           ),
+                          ),
                         ),
+
 
                          // 비밀번호
                         TextFormField(
                           key: ValueKey(4),
-                          validator: (value){
-                            // 유효성검사
-                            if(value!.isEmpty || value.length < 4){
-                              return 'Please enter at least 4 characters';
-                            }
-                            return null;
-                          },
-                          onSaved: (value){
-                            userPassword = value!;
-                          },
+                          // validator: (value){
+                          //   // 유효성검사
+                          //   if(value!.isEmpty || value.length < 4){
+                          //     return 'Please enter at least 4 characters';
+                          //   }
+                          //   return null;
+                          // },
+                          // onSaved: (value){
+                          //   userPassword = value!;
+                          // },
                           onChanged: (value){
                             userPassword = value;
                             if(userPassword.length < 6){
@@ -573,8 +569,9 @@ class _LoginState extends State<Login> {
                         }else{
                           // 사인업 탭이면
                           _tryValudation();
-                          try{
-                            if(userEmail.length>0 && userPassword.length > 6 && userPassword==userPasswordCheck){
+                          try{ // 조건 조정하기, 에러메시지 제대로 출력하기 / 한군데서 관리하기(validator통합)
+                            print('A');
+                            if(userEmail.length>0 && userPassword.length > 5 && userPassword==userPasswordCheck){
                               final newUser = await _authentication.createUserWithEmailAndPassword(
                                 email: userEmail, 
                                 password: userPassword
@@ -588,9 +585,9 @@ class _LoginState extends State<Login> {
                             }
                             
                           }catch(e){
-                            setState(() {
-                              authErrorHandlingfunc(e.toString());
-                            });
+                            // setState(() {
+                              // authErrorHandlingfunc(e.toString());
+                            // });
 
                           }
                         }
